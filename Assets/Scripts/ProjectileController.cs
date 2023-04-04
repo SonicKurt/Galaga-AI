309080b0087553d6996115d2d57a3c95218372ff
Jackson Isenhower <jacksonisenhower@cedarville.edu>
2023-04-04 13:39:16 -0400

    Added targetTag var and OnTriggerEnter method to ProjectileController.cs

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileController : MonoBehaviour
-{
+public class ProjectileController : MonoBehaviour {
     private float speed = 20.0f;
+    private String targetTag = "";
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         if (transform.position.z >= 25.0f) {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.compareTag(targetTag)) {
+            Destroy(other.gameObject);
         }
     }
 }
